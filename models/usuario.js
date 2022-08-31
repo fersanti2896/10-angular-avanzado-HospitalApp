7833a06c5027c57ea13ddd630068abf3e10cf2dd
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-31 11:31:38 -0500

    Terminando la petición GET en usuariosController.js

--- models/usuario.js
+++ models/usuario.js
@@ -30,4 +30,12 @@ const UsuarioSchema = Schema({
     }
 });
 
+/* Es para efectos visuales, no afecta la BD */
+UsuarioSchema.method('toJSON', function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.uid = _id;
+
+    return object;
+});
+
 module.exports = model('Usuario', UsuarioSchema);
